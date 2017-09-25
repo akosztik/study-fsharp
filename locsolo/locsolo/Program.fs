e4ba28c7fdf5473dc764a23294e527e785ef81ec
anett.kosztik.ext <anett.kosztik.ext@evosoft.com>
2017-09-25 15:46:08 +0200

    1.feladat locsolo

---
+++ locsolo/locsolo/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+
+let locsol() =
+    printfn"Kata kannája 2.5 liter 50 tövetlocsol meg vele. 1 tőre 1.5 liter vizet önt."
+    let toszam =50.0
+    let kannatérfogat= 2.5
+    let toszomja= 1.5
+    let fordulat =(int)((toszam*toszomja)/kannatérfogat)
+    printfn " %i -szor megy a kútra." fordulat
+
+locsol()
+Console.ReadKey() |> ignore
