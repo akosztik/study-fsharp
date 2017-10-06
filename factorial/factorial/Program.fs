47ea29cd2e326f54455defffed5106f2b1122a1e
Kosztik Anett <akosztik@gmail.com>
2017-10-06 10:02:42 +0200

    Add files via upload

---
+++ factorial/factorial/Program.fs
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+
+    let fact n =
+        [1..n]
+        |>Seq.fold(fun i t -> i*t) 1
+            
+
+    let factSeq= fact 3
+    printfn "%A" factSeq
+    0 // return an integer exit code
