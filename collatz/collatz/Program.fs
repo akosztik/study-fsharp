1c5f775e67f6e9cec1bb3d679a65c6ec406a3c93
Kosztik Anett <akosztik@gmail.com>
2017-10-06 10:04:08 +0200

    Add files via upload

---
+++ collatz/collatz/Program.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+
+    let collatz n =
+        Seq.unfold (fun i -> 
+        if i % 2 = 0 then Some(i, i / 2) else Some(i, 3 * i + 1)) n
+        
+
+    let aCollatzSeq = collatz 6 |> Seq.takeWhile (fun n -> n <> 1) 
+    printfn "%A" (Seq.toList aCollatzSeq)
+   
+
+    0 // return an integer exit code
