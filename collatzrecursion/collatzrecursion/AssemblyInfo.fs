8aff2d3034cda299ec5b41a7e82f4a2fa6e7f03f
akosztik <akosztik@gmail.com>
2017-10-16 21:56:46 +0200

    added collatz rec

---
+++ collatzrecursion/collatzrecursion/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace collatzrecursion.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("collatzrecursion")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("collatzrecursion")>]
+[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("ddb692a5-a898-470d-ae99-f8c85d79ce3a")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ collatzrecursion/collatzrecursion/Program.fs
@@ -0,0 +1,20 @@
+﻿
+// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+
+    let rec collatz = function
+        | 1 -> [1]
+        | 0 -> [0]
+        | n when n%2=0 -> n::collatz (n/2) 
+        | n when n%2=1 -> n::collatz (3*n+1) 
+
+    let collatzS = collatz 6
+         
+    printfn "%A" (Seq.toList collatzS)
+   
+
+    0 // return an integer exit code
